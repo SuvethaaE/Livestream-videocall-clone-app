53bf91b41ff45d049f2225df9cbaa4834c810985
Suvethaa <suvethaa2001@gmail.com>
2021-07-11 16:25:35 +0530

    my project

--- client/src/SocketContext.js
+++ client/src/SocketContext.js
@@ -4,7 +4,7 @@ import Peer from 'simple-peer';
 
 const SocketContext = createContext();
 
-const socket = io('https://suvecam-video-chat.herokuapp.com');  
+const socket = io('https://livestream-videocall-app.herokuapp.com');  
 //const socket = io('https://warm-wildwood-81069.herokuapp.com');
 
 const ContextProvider = ({ children }) => {
